a8f517b321a84a95ab74e542a9517a6aad38eb42
noormohammad20 <mdsaim204@gmail.com>
2022-04-21 17:22:52 +0600

    Load and display data from database, create delete button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import AddUser from '../AddUser/AddUser'
-
+import React, { useEffect, useState } from 'react'
 const Home = () => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+    const handleUserDelete = id => {
+        const proceed = window.confirm('are you sure !! you want to delete ??')
+        if (proceed) {
+
+            console.log('deleting user with id', id)
+        }
+
+    }
     return (
         <div>
-            <AddUser></AddUser>
+            <h2>Available users {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                        key={user._id}>
+                        {user.name}::{user.email}
+                        <button onClick={() => handleUserDelete(user._id)}>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     )
 }
